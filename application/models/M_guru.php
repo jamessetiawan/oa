6f34272ac524c2e27dc3c16a9520329386feb66d
irwansah <irwansah607@gmail.com>
2021-07-03 11:39:45 +0700

    update retrun

--- application/models/M_guru.php
+++ application/models/M_guru.php
@@ -39,7 +39,7 @@ class M_guru extends CI_Model
 
     function GetDataEmpl()
     {
-        return $this->db->query("select * from employess inner join users on employess.nik=users.nik");
+        $this->db->query("select * from employess inner join users on employess.nik=users.nik");
         return $query->result();
     }
     function GetDataEmpl_a($status)
