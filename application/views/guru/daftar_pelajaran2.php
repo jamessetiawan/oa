a4e76c6396f85a80f76b7bf599e926d2b47d8950
rahmat setiawan <45360516+jamessetiawan@users.noreply.github.com>
2021-07-09 08:56:47 +0700

    update close tag div

--- application/views/guru/daftar_pelajaran2.php
+++ application/views/guru/daftar_pelajaran2.php
@@ -4,35 +4,36 @@
                 date_default_timezone_set("Asia/Jakarta");
                 echo date("l, d F Y H:i:s");
                 ?>
-                <div class="table-responsive">
-                  <table class="table datatable no-margin">
-                    <thead>
-                      <tr>
-                        <th>No</th>
-                        <th>Nama Matapelajaran</th>
-                        <th>Keterangan</th>
-                      </tr>
-                    </thead>
-                    <tbody>
+              </div>
+              <div class="table-responsive">
+                <table class="table datatable no-margin">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Nama Matapelajaran</th>
+                      <th>Keterangan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
 
-                      <?php
-                      if (!empty($GetMengajar)) {
-                        $no = 1;
-                        foreach ($GetMengajar as $GetMengajar_read) {
-                      ?>
-                          <tr>
-                            <td><?php echo $no++; ?></td>
-                            <td><?php echo $GetMengajar_read->name; ?></td>
-                            <td><a href="<?php echo site_url('Guru/daftar_kelas2/' . $GetMengajar_read->id); ?>">Lihat Kelas</a></td>
-                          </tr>
-                      <?php
+                    <?php
+                    if (!empty($GetMengajar)) {
+                      $no = 1;
+                      foreach ($GetMengajar as $GetMengajar_read) {
+                    ?>
+                        <tr>
+                          <td><?php echo $no++; ?></td>
+                          <td><?php echo $GetMengajar_read->name; ?></td>
+                          <td><a href="<?php echo site_url('Guru/daftar_kelas2/' . $GetMengajar_read->id); ?>">Lihat Kelas</a></td>
+                        </tr>
+                    <?php
 
-                        }
                       }
-                      ?>
+                    }
+                    ?>
 
 
-                    </tbody>
-                  </table>
-                </div>
-                <!-- /.table-responsive -->
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.table-responsive -->
