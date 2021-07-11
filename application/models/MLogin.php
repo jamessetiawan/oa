88faf773f08b428600cb94c4a3a36a7539d8fce6
rahmat setiawan <45360516+jamessetiawan@users.noreply.github.com>
2021-07-11 20:59:09 +0700

    update fonts, name field in daftarsiswa2

--- application/models/MLogin.php
+++ application/models/MLogin.php
@@ -1,48 +1,49 @@
 <?php
-	class MLogin extends CI_Model{
-		function __construct(){
+class MLogin extends CI_Model
+{
+	function __construct()
+	{
 		parent::__construct();
-		}
-
-		function GoLogin($email,$password){
-			$this->db->select('*');
-			$this->db->from('users');
-			$this->db->where('email', $email);
-			$this->db->or_where('users.nik =', $email); 
-			$this->db->where('password', $password);
-			
-			$query = $this->db->get();
-			
-			if($query -> num_rows() == 1){
-				$row = $query->row(); 
-				$this->session->set_userdata('nik',$row->nik);
-				$this->session->set_userdata('email',$row->email);
-				$this->session->set_userdata('username',$row->username);	
-				$this->session->set_userdata('type',$row->type);
-				$this->session->set_userdata('id_user',$row->id);
-
-				$empl=$this->db->get_where('employess',array('nik' => $row->nik))->row();
-				if($empl){
-					$this->session->set_userdata('position',$empl->position);	
-				}else{
-					$this->session->set_userdata('position',$row->email);	
-				}
-
-				$this->db->select('*');
-				$this->db->from('school_year');
-				$this->db->where('status',1);
-				$sql=$this->db->get();
-				$ta=$sql->row();
-				
-		
-				$this->session->set_userdata('school_year_id',$ta->id);
-				$this->session->set_userdata('school_year_nama',$ta->first);
-    			
-    			return $row->nik;
-			}else{
-				return false;
-			}
-		}
-
 	}
-?>
+
+	function GoLogin($email, $password)
+	{
+		$this->db->select('*');
+		$this->db->from('users');
+		$this->db->where('email', $email);
+		$this->db->or_where('users.nik =', $email);
+		$this->db->where('password', $password);
+
+		$query = $this->db->get();
+
+		if ($query->num_rows() == 1) {
+			$row = $query->row();
+			$this->session->set_userdata('nik', $row->nik);
+			$this->session->set_userdata('email', $row->email);
+			$this->session->set_userdata('username', $row->username);
+			$this->session->set_userdata('type', $row->type);
+			$this->session->set_userdata('id_user', $row->id);
+
+			$empl = $this->db->get_where('employess', array('nik' => $row->nik))->row();
+			if ($empl) {
+				$this->session->set_userdata('position', $empl->position);
+			} else {
+				$this->session->set_userdata('position', $row->email);
+			}
+
+			$this->db->select('*');
+			$this->db->from('school_year');
+			$this->db->where('status', 1);
+			$sql = $this->db->get();
+			$ta = $sql->row();
+
+
+			$this->session->set_userdata('school_year_id', $ta->id);
+			$this->session->set_userdata('school_year_nama', $ta->first);
+
+			return $row->nik;
+		} else {
+			return false;
+		}
+	}
+}
--- application/views/guru/daftar_siswa.php
+++ application/views/guru/daftar_siswa.php
@@ -1,81 +1,76 @@
 <form action="<?php echo site_url('Guru/SimpanAbsensi'); ?>" method="post">
-              <div class="table-responsive">
-                <table class="table datatable no-margin">
-                  <thead>
-                  <tr>
-                      <td colspan="4">
-                        <select name="pert" class="form-control" required>
-                          <option value="">Silahkan Pilih Pertemuan Ke ...</option>
-                           <?php
-                           $awal=1;
-                     if(!empty($GetCekPertemuan))
-                     {
-                          $awal=1;
-                        foreach($GetCekPertemuan as $GetCekPertemuan_read) 
-                        {
-                          $awal=$awal+1;  
-                        }
-                    }
-                    ?>
-                          <?php
-                          
-                            $no=1;
-                            for($i=$awal;$i<=16;$i++)
-                            {
-                          ?>
-                          <option value="<?php echo $i; ?>">Pertemuan <?php echo $i; ?></option>
-                          <?php
-                            }
-                          ?>
-                        </select>
-                        <input type="hidden" name="subject_teacher_id" class="form-control" value="<?php echo $this->uri->segment(3); ?>">
-                         <input type="hidden" name="class_room_id" class="form-control" value="<?php echo $this->uri->segment(4); ?>">
-                      </td>
-                  </tr>
-                  <tr>
-                    <th>No</th>
-                    <th>NIS</th>
-                    <th>Nama Siswa</th>
-                    <th>Keterangan</th>
-                  </tr>
-                  </thead>
-                  <tbody>
+  <div class="table-responsive">
+    <table class="table table-hover datatable no-margin">
+      <thead>
+        <tr>
+          <td colspan="4">
+            <select name="pert" class="form-control" required>
+              <option value="">Pilih pertemuan</option>
+              <?php
+              $awal = 1;
+              if (!empty($GetCekPertemuan)) {
+                $awal = 1;
+                foreach ($GetCekPertemuan as $GetCekPertemuan_read) {
+                  $awal = $awal + 1;
+                }
+              }
+              ?>
+              <?php
 
-                    <?php
-                     if(!empty($GetDaftarSiswa))
-                     {
-                          $no=1;
-                        foreach($GetDaftarSiswa as $GetDaftarSiswa_read) 
-                        { 
-                    ?>
-                          <tr>
-                            <td><?php echo $no++; ?></td>
-                            <td><?php echo $GetDaftarSiswa_read->nis; ?>
-                              <input type="hidden" class="form-control" value="<?php echo $GetDaftarSiswa_read->nis; ?>" name="nis<?php echo $no-1; ?>">
-                            </td>
-                            <td><?php echo $GetDaftarSiswa_read->name; ?></td>
-                            <td>
-                              <select name="attendence<?php echo $no-1; ?>" class="form-control">
-                                <option value="1">Masuk</option>
-                                <option value="2">Izin/Sakit</option>
-                                <option value="0">Tidak Masuk</option>
-                              </select>
-                            </td>
-                          </tr>
-                  <?php
-                          
-                      }
-                    }
-                  ?>
-                        <tr>
-                          <td colspan="4">
-                            <input type="hidden" name="jumlah" value="<?php echo $no-1; ?>">
-                            <button type="submit" name="simpan" class="form-control">Simpan</button>
-                          </td>
-                        </tr>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.table-responsive -->
-          
+              $no = 1;
+              for ($i = $awal; $i <= 16; $i++) {
+              ?>
+                <option value="<?php echo $i; ?>">Pertemuan <?php echo $i; ?></option>
+              <?php
+              }
+              ?>
+            </select>
+            <input type="hidden" name="subject_teacher_id" class="form-control" value="<?php echo $this->uri->segment(3); ?>">
+            <input type="hidden" name="class_room_id" class="form-control" value="<?php echo $this->uri->segment(4); ?>">
+          </td>
+        </tr>
+        <tr>
+          <th>No</th>
+          <th>NIS</th>
+          <th>Nama Siswa</th>
+          <th>Keterangan</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        <?php
+        if (!empty($GetDaftarSiswa)) {
+          $no = 1;
+          foreach ($GetDaftarSiswa as $GetDaftarSiswa_read) {
+        ?>
+            <tr>
+              <td><?php echo $no++; ?></td>
+              <td><?php echo $GetDaftarSiswa_read->nis; ?>
+                <input type="hidden" class="form-control" value="<?php echo $GetDaftarSiswa_read->nis; ?>" name="nis<?php echo $no - 1; ?>">
+              </td>
+              <td><?php echo $GetDaftarSiswa_read->name; ?></td>
+              <td>
+                <select name="attendence<?php echo $no - 1; ?>" class="form-control">
+                  <option value="1">Masuk</option>
+                  <option value="2">Izin/Sakit</option>
+                  <option value="0">Tidak Masuk</option>
+                </select>
+              </td>
+            </tr>
+        <?php
+
+          }
+        }
+        ?>
+        <tr>
+          <td colspan="4">
+            <input type="hidden" name="jumlah" value="<?php echo $no - 1; ?>">
+            <button type="submit" name="simpan" class="form-control btn btn-primary">Simpan</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <!-- /.table-responsive -->
+
 </form>
